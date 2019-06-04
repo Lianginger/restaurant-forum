eee88ef0b277f6df70230043dd60ca882d9d99c9
Lianginger <jackyliang444@hotmail.com.tw>
2019-06-04 14:38:46 +0800

    Add basic controller

--- app.js
+++ app.js
@@ -3,13 +3,12 @@ const app = express()
 const port = 3000
 const handlebars = require('express-handlebars')
 
-app.engine('handlebars', handlebars())
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
-
-app.get('/', (req, res) => {
-  res.render('home')
-})
 
 app.listen(port, () => {
   console.log(`App is running on ${port}`)
 })
+
+
+require('./routes')(app)
---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const restController = require('../controllers/restController')
+const adminController = require('../controllers/adminController')
+
+module.exports = app => {
+  //如果使用者訪問首頁，就導向 /restaurants 的頁面
+  app.get('/', (req, res) => res.redirect('/restaurants'))
+  app.get('/restaurants', restController.getRestaurants)
+
+  app.get('/admin', (req, res) => res.redirect('/admin/restaurants'))
+  app.get('/admin/restaurants', adminController.getRestaurants)
+}
