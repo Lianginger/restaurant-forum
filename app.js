b1c9f4894d84c37317f942f899ba6febd0a133bc
lianginger <jackyliang444@hotmail.com.tw>
2019-06-05 00:37:13 +0800

    Add sequilize

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 const handlebars = require('express-handlebars')
+const db = require('./models')
 
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 app.listen(port, () => {
+  db.sequelize.sync()
   console.log(`App is running on ${port}`)
 })
 
