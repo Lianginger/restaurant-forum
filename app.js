ce50d87b31f0ac380f097db14441e132d86d6d9c
lianginger <jackyliang444@hotmail.com.tw>
2019-06-07 13:08:22 +0800

    Add userController and signup page

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const app = express()
 const port = 3000
 const handlebars = require('express-handlebars')
 const db = require('./models')
+const bodyParser = require('body-parser')
 
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(port, () => {
   db.sequelize.sync()
