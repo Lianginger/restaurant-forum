c7915640921c770c861e602e1f8d5d89c70cf08c
lianginger <jackyliang444@hotmail.com.tw>
2019-06-07 17:36:02 +0800

    Add admin restaurant post feature

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -6,7 +6,24 @@ const adminController = {
     Restaurant.findAll().then(restaurants => {
       res.render('admin/restaurants', { restaurants })
     })
-  }
+  },
+
+  createRestaurant: (req, res) => {
+    res.render('admin/create')
+  },
+
+  postRestaurant: (req, res) => {
+    if (!req.body.name) {
+      req.flash('error_messages', '餐廳名稱不存在！')
+      res.redirect('back')
+    } else {
+      Restaurant.create(req.body)
+        .then(restaurant => {
+          req.flash('success_messages', `餐廳 ${restaurant.name} 新增成功！`)
+          res.redirect('/admin/restaurants')
+        })
+    }
+  },
 }
 
 module.exports = adminController
