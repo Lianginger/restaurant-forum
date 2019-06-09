403b81ea3acd3a506087a5eff2046e8873ae0101
lianginger <jackyliang444@hotmail.com.tw>
2019-06-09 16:58:22 +0800

    Install Imgur api and update admin POST and PUT

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,6 +1,8 @@
 const db = require('../models')
 const Restaurant = db.Restaurant
 const fs = require('fs')
+const imgur = require('imgur-node-api')
+const IMGUR_CLIENT_ID = 'a6377f1810d0270'
 
 const adminController = {
   getRestaurants: (req, res) => {
@@ -29,24 +31,22 @@ const adminController = {
       res.redirect('back')
     } else {
       const { file } = req
-      console.log(req.body)
-      console.log(file)
+
       if (file) {
-        fs.readFile(file.path, (err, data) => {
-          if (err) console.log('Error: ', err)
-          fs.writeFile(`upload/${file.originalname}`, data, () => {
-            Restaurant.findByPk(req.params.id)
-              .then(restaurant => {
-                restaurant.update(req.body)
-                restaurant.image = `/upload/${file.originalname}`
-                restaurant.save()
-                  .then(restaurant => {
-                    req.flash('success_messages', `餐廳 ${restaurant.name} 更新成功！`)
-                    res.redirect('/admin/restaurants')
-                  })
-              })
-          })
+        imgur.setClientID(IMGUR_CLIENT_ID)
+        imgur.upload(file.path, (err, img) => {
+          Restaurant.findByPk(req.params.id)
+            .then(restaurant => {
+              restaurant.update(req.body)
+              restaurant.image = img.data.link
+              restaurant.save()
+                .then(restaurant => {
+                  req.flash('success_messages', `餐廳 ${restaurant.name} 更新成功！`)
+                  res.redirect('/admin/restaurants')
+                })
+            })
         })
+
       } else {
         Restaurant.findByPk(req.params.id)
           .then(restaurant => {
@@ -71,23 +71,20 @@ const adminController = {
       res.redirect('back')
     } else {
       const { file } = req
-      console.log(req.body)
-      console.log(file)
+
       if (file) {
-        fs.readFile(file.path, (err, data) => {
-          if (err) console.log('Error: ', err)
-          fs.writeFile(`upload/${file.originalname}`, data, () => {
-            Restaurant.create({
-              name: req.body.name,
-              tel: req.body.tel,
-              address: req.body.address,
-              opening_hours: req.body.opening_hours,
-              description: req.body.description,
-              image: `/upload/${file.originalname}`
-            }).then(restaurant => {
-              req.flash('success_messages', `餐廳 ${restaurant.name} 新增成功！`)
-              res.redirect('/admin/restaurants')
-            })
+        imgur.setClientID(IMGUR_CLIENT_ID)
+        imgur.upload(file.path, (err, img) => {
+          Restaurant.create({
+            name: req.body.name,
+            tel: req.body.tel,
+            address: req.body.address,
+            opening_hours: req.body.opening_hours,
+            description: req.body.description,
+            image: img.data.link
+          }).then(restaurant => {
+            req.flash('success_messages', `餐廳 ${restaurant.name} 新增成功！`)
+            res.redirect('/admin/restaurants')
           })
         })
       } else {
