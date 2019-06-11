8151c5e2141153a566bce016e6cc6cdcbeb79407
Lianginger <jackyliang444@hotmail.com.tw>
2019-06-11 13:45:43 +0800

    Add admin category post feature

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -5,6 +5,18 @@ let categoryController = {
     Category.findAll().then(categories => {
       res.render('admin/categories', { categories })
     })
+  },
+
+  postCategory: (req, res) => {
+    if (!req.body.name) {
+      req.flash('error_messages', '類別名稱不存在！')
+      res.redirect('back')
+    } else {
+      Category.create(req.body)
+        .then(category => {
+          res.redirect('/admin/categories')
+        })
+    }
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -40,7 +40,7 @@ module.exports = (app, passport) => {
   app.delete('/admin/restaurants/:id', authenticatedAdmin, adminController.deleteRestaurant)
 
   app.get('/admin/categories', authenticatedAdmin, categoryController.getCategories)
-  // app.post('/admin/categories', authenticatedAdmin, categoryController.postCategory)
+  app.post('/admin/categories', authenticatedAdmin, categoryController.postCategory)
   // app.get('/admin/categories/:id', authenticatedAdmin, categoryController.getCategories)
   // app.put('/admin/categories/:id', authenticatedAdmin, categoryController.putCategory)
   // app.delete('/admin/categories/:id', authenticatedAdmin, categoryController.deleteCategory)
