1b47062e9818ce0603a546c4383eab8387c4a8f2
Lianginger <jackyliang444@hotmail.com.tw>
2019-07-09 17:39:45 +0800

    Refactor categoryController.getCategory

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,16 +1,11 @@
 const db = require('../models')
 const Category = db.Category
+const categoryService = require('../services/categoryService')
+
 let categoryController = {
   getCategories: (req, res) => {
-    Category.findAll().then(categories => {
-      if (req.params.id) {
-        Category.findByPk(req.params.id)
-          .then(category => {
-            res.render('admin/categories', { categories, category })
-          })
-      } else {
-        res.render('admin/categories', { categories })
-      }
+    categoryService.getCategories(req, res, data => {
+      res.render('admin/categories', data)
     })
   },
 
@@ -24,7 +19,7 @@ let categoryController = {
           req.flash('success_messages', `類別 ${req.body.name} 新增完成！`)
           res.redirect('/admin/categories')
         })
-        .catch(function (err) {
+        .catch(function(err) {
           // print the error details
           console.log(err)
         })
@@ -36,25 +31,22 @@ let categoryController = {
       req.flash('error_messages', '類別名稱不存在！')
       res.redirect('back')
     } else {
-      Category.findByPk(req.params.id)
-        .then(category => {
-          category.update(req.body)
-            .then(() => {
-              req.flash('success_messages', `類別 ${req.body.name} 更新完成！`)
-              res.redirect('/admin/categories')
-            })
+      Category.findByPk(req.params.id).then(category => {
+        category.update(req.body).then(() => {
+          req.flash('success_messages', `類別 ${req.body.name} 更新完成！`)
+          res.redirect('/admin/categories')
         })
+      })
     }
   },
 
   deleteCategory: (req, res) => {
-    Category.findByPk(req.params.id)
-      .then(category => {
-        category.destroy().then(() => {
-          req.flash('success_messages', `類別 ${category.name} 刪除完成！`)
-          res.redirect('/admin/categories')
-        })
+    Category.findByPk(req.params.id).then(category => {
+      category.destroy().then(() => {
+        req.flash('success_messages', `類別 ${category.name} 刪除完成！`)
+        res.redirect('/admin/categories')
       })
+    })
   }
 }
 
